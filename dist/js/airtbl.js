23f8ce1146a93456f79cba25d7cee9288cc36e4c
ZolF-werk <zolf@bk.ru>
2021-08-27 18:16:02 +0300

    almostModal

--- dist/js/airtbl.js
+++ dist/js/airtbl.js
@@ -24,23 +24,19 @@ async function getTechs() {
 function mapApparatus() {
     // 	тут перебираем аппараты и добавляем характеристики
 
-    cloneGearDivs(apparatus); // Перебор массива//////////////////////////////////////////
+    cloneGearTemplates(apparatus);
     //cloneGearDivs(techs); // Перебор массива///////////////////////////////////////////
+    createGearItems();
+    glider.refresh(true);
 }
 
-function cloneGearDivs(geck) {
+function cloneGearTemplates(apparatusArr) {
     const gliderDiv = document.querySelector('.glider-track');
-    const gear1 = document.querySelector('div.gears-wrap._desktop');
-    const gearS = document.querySelectorAll('.gears-wrap');
-    const count = geck.records.length;
+    const count = apparatusArr.records.length;
+    const template = document.querySelector('#gearsTemp');
     for (let i = 0; i < count; i++) {
-        let el = gearS[i];
-        el.classList.remove('disabled');
-        gliderDiv.appendChild(gear1.cloneNode(true));
-        el.classList.remove('disabled');
+        gliderDiv.append(template.content.cloneNode(true));
     }
-
-    createGearItems();
 }
 
 function createGearItems() {
@@ -71,19 +67,8 @@ function createGearItems() {
     }
 }
 
-function onBtnInfo() {
-    const btnInfo = document.querySelectorAll('.btn_info');
-
-    [...btnInfo].forEach((btnInfo) =>
-        btnInfo.addEventListener('click', () => {
-            gearItem = btnInfo.closest('div[data-id]');
-            console.log(btnInfo.closest('div[data-id]'));
-        })
-    );
-}
-
 /////////////Ф-я запуска модального окно с любой кнопки
-function modalWindowOpen() {
+function openModalWindow() {
     console.log(document.querySelectorAll('.btn_info').length);
     let popupLinks = document.querySelectorAll('.btn_info');
     for (let btnModal of popupLinks) {
@@ -99,6 +84,67 @@ function modalWindowOpen() {
 function fillModalWindow(idGear) {
     console.log('PopUp  working', idGear);
     const popUp = document.querySelector('.popup');
+    let iEl = 0;
+    let iFor = 0;
+    let gearNameArr = [];
+    ////////счётчик на кол-во записей в techs
+    for (let i = 0; i < techs.records.length; i++) {
+        const appId = techs.records[i].fields.apparatus[0];
+        const techsRec = techs.records[i];
+        ///////////////////////Если id кофемашин совпадают
+        if (idGear === appId) {
+            // console.log(techs.records[i].fields.Name + techs.records[i].fields.value);
+
+            gearNameArr.push(techs.records[i].fields.Name);
+
+            techs.records[i].fields.Name;
+            techs.records[i].fields.value;
+            // document.createElement('div').classList.add('popup__tech_item');
+            // document.createElement('div').classList.add('popup__tech_name');
+            // document.createElement('div').classList.add('popup__tech_num');
+
+            pTechWrap = 'popup__tech_wrap';
+            pTechItem = 'popup__tech_item';
+            pTechName = 'popup__tech_name';
+            pTechNum = 'popup__tech_num';
+            arrTechItem = document.querySelectorAll('.popup__tech_item');
+
+            createElemModalWindow(pTechItem, pTechWrap);
+            createElemModalWindow(pTechName, arrTechItem[iEl]);
+            createElemModalWindow(pTechNum, arrTechItem[iEl]);
+
+            // techs.records[i].fields.Name === 'Максимальное давление';
+            // let popupTechWrap = document.querySelector('.popup__tech_wrap');
+            // popupTechWrap.insertAdjacentHTML(
+            //     'beforeend',
+            //     `<div class="popup__tech_item"><div class="popup__tech_name _maxpresure">Максимальное давление!</div><div class="popup__tech_num _maxpresure_sub">15 Бар</div></div>`
+            // );
+            iEl++;
+        }
+    }
+}
+
+function createElemModalWindow(_classNameChild, _classNameParent) {
+    popupTechX = document.querySelector(`.${_classNameParent}`);
+    g = document.createElement('div');
+    g.className = _classNameChild;
+    popupTechX.append(g);
+}
+function arrOfpTechItem() {
+    arrTechItem = document.querySelectorAll('.popup__tech_item');
+}
+
+getApparatus().then(getTechs).then(mapApparatus); ////////////////////////////////////
+setTimeout(() => {
+    openModalWindow();
+    console.log('Заменить Таймер на промис/async');
+}, 1300);
+
+///////////////////glider.addItem
+
+function fillModalWindowBack(idGear) {
+    console.log('PopUp  working', idGear);
+    const popUp = document.querySelector('.popup');
 
     for (let i = 0; i < techs.records.length; i++) {
         const appId = techs.records[i].fields.apparatus[0];
@@ -106,19 +152,13 @@ function fillModalWindow(idGear) {
 
         if (idGear === appId) {
             console.log(techs.records[i].fields.Name);
-            if (techs.records[0].fields.Name === 'Максимальное давление') {
+            if (techs.records[i].fields.Name === 'Максимальное давление') {
                 let popupTechWrap = document.querySelector('.popup__tech_wrap');
-                let popupTechItem = popupTechWrap.insertAdjacentHTML(
-                    '',
-                    '<div class="popup__tech_item"><div class="popup__tech_name _maxpresure">Максимальное давление</div><div class="popup__tech_num _maxpresure_sub">15 Бар</div></div>'
+                popupTechWrap.insertAdjacentHTML(
+                    'beforeend',
+                    `<div class="popup__tech_item"><div class="popup__tech_name _maxpresure">Максимальное давление!</div><div class="popup__tech_num _maxpresure_sub">15 Бар</div></div>`
                 );
             }
         }
     }
 }
-
-getApparatus().then(getTechs).then(mapApparatus); ////////////////////////////////////
-setTimeout(() => {
-    modalWindowOpen();
-    console.log('Заменить Таймер на промис/async');
-}, 2000);
